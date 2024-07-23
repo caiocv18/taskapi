6c6896355ddfda97fda83be6bd4292f4ffb73fb6
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-07-23 11:05:41 -0300

    Creating Models

---
+++ src/main/java/com/caiocv18/taskapi/model/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.caiocv18.taskapi.model;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
